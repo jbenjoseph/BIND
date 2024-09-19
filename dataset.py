1f9e39af53d8e7166a232854770b70864b199acb
JJ Ben-Joseph <jj@tensorspace.ai>
2024-09-19 11:03:05 +0300

    Use PyTorch Lightning for multiGPU support

--- dataset.py
+++ dataset.py
@@ -1,19 +1,17 @@
 import json
 import random
 import math
-
 from tqdm import tqdm
-
 import logging
-logging.getLogger("pysmiles").setLevel(logging.CRITICAL)
+import torch
+from torch_geometric.utils.sparse import dense_to_sparse
+from torch_geometric.data import Data
+from torch.utils.data import Dataset
 
 import loading
 from data import BondType
 
-import torch
-
-from torch_geometric.utils.sparse import dense_to_sparse
-from torch_geometric.data import Data
+logging.getLogger("pysmiles").setLevel(logging.CRITICAL)
 
 # Optimisation
 length_cutoff = 2048
@@ -24,20 +22,28 @@ main_database = json.loads(open("examples/bindingdb_sample.json").read())
 all_smiles_pairs = dict()
 
 for key in main_database.keys():
-
     sequence = main_database[key]["sequence"].upper()
-
-    if sequence not in all_smiles_pairs.keys():
-        all_smiles_pairs[sequence] = list()
-
+    if sequence not in all_smiles_pairs:
+        all_smiles_pairs[sequence] = []
     all_smiles_pairs[sequence].append(main_database[key]["smiles"])
 
 all_smiles = list(set([x["smiles"] for x in main_database.values()]))
 
-def get_graph (smiles):
 
-    graph = loading.get_data(smiles, apply_paths=False, parse_cis_trans=False, unknown_atom_is_dummy=True)
-    x, a, e = loading.convert(*graph, bonds=[BondType.SINGLE, BondType.DOUBLE, BondType.TRIPLE, BondType.AROMATIC, BondType.NOT_CONNECTED])
+def get_graph(smiles):
+    graph = loading.get_data(
+        smiles, apply_paths=False, parse_cis_trans=False, unknown_atom_is_dummy=True
+    )
+    x, a, e = loading.convert(
+        *graph,
+        bonds=[
+            BondType.SINGLE,
+            BondType.DOUBLE,
+            BondType.TRIPLE,
+            BondType.AROMATIC,
+            BondType.NOT_CONNECTED,
+        ],
+    )
 
     x = torch.Tensor(x)
     a = dense_to_sparse(torch.Tensor(a))[0]
@@ -45,24 +51,18 @@ def get_graph (smiles):
 
     # Given an xae
     graph = Data(x=x, edge_index=a, edge_features=e)
-
     return graph
+
 
 smiles_graphs = dict()
 
 for allowed_smiles in tqdm(all_smiles):
-
     try:
-
         graph = get_graph(allowed_smiles)
-
         if graph.edge_index.shape[1] == 0:
             continue
-
         smiles_graphs[allowed_smiles] = graph
-
-    except:
-
+    except Exception as e:
         continue
 
 allowed_smiles_set = set(smiles_graphs.keys())
@@ -71,140 +71,107 @@ allowed_smiles_list = list(allowed_smiles_set)
 current_set = [x for x in main_database.items() if x[1]["smiles"] in allowed_smiles_set]
 
 for key in main_database.keys():
-
     main_database[key]["length"] = len(main_database[key]["sequence"])
 
 print("Reformatting main database")
 
 random.Random(0).shuffle(current_set)
 
-train_set = current_set[:math.floor(0.9 * len(current_set))]
-validation_set = current_set[math.floor(0.9 * len(current_set)):math.floor(0.92 * len(current_set))]
-test_set = current_set[math.floor(0.92 * len(current_set)):]
+train_set = current_set[: math.floor(0.9 * len(current_set))]
+validation_set = current_set[
+    math.floor(0.9 * len(current_set)) : math.floor(0.92 * len(current_set))
+]
+test_set = current_set[math.floor(0.92 * len(current_set)) :]
 
-train_dataset = {x[0]: x[1] for x in train_set}
-validation_dataset = {x[0]: x[1] for x in validation_set}
+train_dataset_dict = {x[0]: x[1] for x in train_set}
+validation_dataset_dict = {x[0]: x[1] for x in validation_set}
 
-train_range = [(x, y) for x, y in train_dataset.items() if y["length"] < length_cutoff]
-validation_range = [(x, y) for x, y in validation_dataset.items() if y["length"] < length_cutoff]
+train_range = [
+    (x, y) for x, y in train_dataset_dict.items() if y["length"] < length_cutoff
+]
+validation_range = [
+    (x, y) for x, y in validation_dataset_dict.items() if y["length"] < length_cutoff
+]
 
 print(len(train_range), "training datapoints loaded")
 print(len(validation_range), "validation datapoints loaded")
 print(len(allowed_smiles_list), "unique SMILES loaded")
 
-def get_train_batch (amount=16, false_ratio=0.5):
 
-    proteins = random.choices(train_range, k=amount)
+class BindingDBDataset(Dataset):
+    def __init__(
+        self,
+        data_range,
+        smiles_graphs,
+        allowed_smiles_list,
+        all_smiles_pairs,
+        is_train=True,
+    ):
+        self.data_range = data_range
+        self.smiles_graphs = smiles_graphs
+        self.allowed_smiles_list = allowed_smiles_list
+        self.all_smiles_pairs = all_smiles_pairs
+        self.is_train = is_train
+        self.false_ratio = 0.5
 
-    ret = list()
+    def __len__(self):
+        return len(self.data_range)
 
-    for protein in proteins:
-
+    def __getitem__(self, idx):
+        protein = self.data_range[idx]
         seq_info = protein[1]
-
         sequence = seq_info["sequence"].upper()
         smiles = seq_info["smiles"]
 
-        binding_ligands_set = set(all_smiles_pairs[sequence])
+        binding_ligands_set = set(self.all_smiles_pairs[sequence])
 
-        is_false_ligand = False
+        is_false_ligand = 0.0
 
-        if random.random() < false_ratio:
-
+        if random.random() < self.false_ratio:
             found = False
             max_tries = 100
-
             while not found and max_tries > 0:
-
                 max_tries -= 1
-
                 try:
-
-                    random_smiles = random.choice(allowed_smiles_list)
-
+                    random_smiles = random.choice(self.allowed_smiles_list)
                     if random_smiles in binding_ligands_set:
                         continue
-
                     smiles = random_smiles
                     found = True
-
-                except:
-
+                except Exception:
                     continue
+            if found:
+                is_false_ligand = 1.0
 
-            is_false_ligand = found
-
-        graph = smiles_graphs[smiles]
+        graph = self.smiles_graphs[smiles]
 
         if is_false_ligand:
-
             ki, ic50, kd, ec50 = [-9999] * 4
-
         else:
+            ki, ic50, kd, ec50 = [
+                x if x is not None else -9999 for x in seq_info["log10_affinities"]
+            ]
 
-            ki, ic50, kd, ec50 = [x if x != None else -9999 for x in seq_info["log10_affinities"]]
-        
-        is_false_ligand = float(is_false_ligand)
+        sample = {
+            "sequence": sequence,
+            "graph": graph,
+            "ki": ki,
+            "ic50": ic50,
+            "kd": kd,
+            "ec50": ec50,
+            "is_false_ligand": is_false_ligand,
+        }
 
-        ret.append([sequence, graph, ki, ic50, kd, ec50, is_false_ligand])
+        return sample
 
-    return ret
 
-def get_validation_batch (amount=16, false_ratio=0.5):
-
-    proteins = random.choices(validation_range, k=amount)
-
-    ret = list()
-
-    for protein in proteins:
-
-        seq_info = protein[1]
-
-        sequence = seq_info["sequence"].upper()
-        smiles = seq_info["smiles"]
-
-        binding_ligands = all_smiles_pairs[sequence]
-        binding_ligands_set = set(binding_ligands)
-
-        is_false_ligand = False
-
-        if random.random() < false_ratio:
-
-            found = False
-            max_tries = 100
-
-            while not found and max_tries > 0:
-
-                max_tries -= 1
-
-                try:
-
-                    random_smiles = random.choice(allowed_smiles_list)
-
-                    if random_smiles in binding_ligands_set:
-                        continue
-
-                    smiles = random_smiles
-                    found = True
-
-                except:
-
-                    continue
-
-            is_false_ligand = found
-
-        graph = smiles_graphs[smiles]
-
-        if is_false_ligand:
-
-            ki, ic50, kd, ec50 = [-9999] * 4
-
-        else:
-
-            ki, ic50, kd, ec50 = [x if x != None else -9999 for x in seq_info["log10_affinities"]]
-        
-        is_false_ligand = float(is_false_ligand)
-
-        ret.append([sequence, graph, ki, ic50, kd, ec50, is_false_ligand])
-
-    return ret
+train_dataset = BindingDBDataset(
+    train_range, smiles_graphs, allowed_smiles_list, all_smiles_pairs, is_train=True
+)
+validation_dataset = BindingDBDataset(
+    validation_range,
+    smiles_graphs,
+    allowed_smiles_list,
+    all_smiles_pairs,
+    is_train=False,
+)
--- train_graph.py
+++ train_graph.py
@@ -1,231 +1,211 @@
 import numpy as np
-
 import torch
 from torch import nn
+import pytorch_lightning as pl
 from transformers import AutoModel, AutoTokenizer
+from torch_geometric.data import Batch
+from sklearn.metrics import average_precision_score, roc_auc_score, accuracy_score
+from torch.utils.data import DataLoader
+from torch.utils.data.distributed import DistributedSampler
 
-import dataset as dataset
+import dataset
 from model_graph import ConditionModel
 
-from sklearn.metrics import average_precision_score, roc_auc_score, accuracy_score
 
-from torch_geometric.data import Batch
-
-from torch.utils.tensorboard import SummaryWriter
-writer = SummaryWriter()
-
-esm_device = torch.device("cpu")
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-esm_tokeniser = AutoTokenizer.from_pretrained("facebook/esm2_t33_650M_UR50D")
-esm_model = AutoModel.from_pretrained("facebook/esm2_t33_650M_UR50D")
-
-esm_model.to(esm_device)
-esm_model.eval()
-
-iterations = 100000
-learning_rate = 1e-4
-batch_size = 1
-grad_accumulation_steps = 256
-
-class MaskLoss (torch.nn.Module):
-
-    def __init__ (self, loss):
+class MaskLoss(nn.Module):
+    def __init__(self, loss):
         super().__init__()
-
         self.loss = loss
 
-    def forward (self, yhat, y):
-
-        y = torch.Tensor(y).to(device)
-        mask = (y > -999).float().to(device)
-
+    def forward(self, yhat, y):
+        mask = (y > -999).float()
         loss = self.loss(yhat.float(), y) * mask
-
         loss = torch.sum(loss)
         num_non_zero = torch.sum(mask)
+        return (
+            loss / num_non_zero
+            if num_non_zero > 0
+            else torch.tensor(0.0, device=y.device)
+        )
 
-        return loss / num_non_zero if num_non_zero > 0 else torch.tensor(0.0)
 
-model = ConditionModel()
-loss_classifier = nn.BCEWithLogitsLoss()
+class ConditionModelLightning(pl.LightningModule):
+    def __init__(self):
+        super().__init__()
+        self.model = ConditionModel()
+        self.loss_fn = MaskLoss(nn.HuberLoss(delta=2.0, reduction="none"))
+        self.mse_loss_fn = MaskLoss(nn.MSELoss(reduction="none"))
+        self.loss_classifier = nn.BCEWithLogitsLoss()
+        self.learning_rate = 1e-4
+        self.iterations = 100000
+        self.esm_tokenizer = AutoTokenizer.from_pretrained(
+            "facebook/esm2_t33_650M_UR50D"
+        )
+        self.esm_model = AutoModel.from_pretrained("facebook/esm2_t33_650M_UR50D")
+        self.esm_model.eval()
 
-loss = MaskLoss(nn.HuberLoss(delta=2.0, reduction="none"))
-mse_loss = MaskLoss(nn.MSELoss(reduction="none"))
+    def forward(self, sequences, graphs):
+        encoded_input = self.esm_tokenizer(
+            sequences, padding="longest", return_tensors="pt"
+        )
+        esm_output = self.esm_model(
+            **encoded_input.to(self.device), output_hidden_states=True
+        )
+        hidden_states = [x.detach().to(self.device) for x in esm_output.hidden_states]
+        attention_mask = encoded_input["attention_mask"].to(self.device)
+        return self.model(graphs, hidden_states, attention_mask)
 
-loss.to(device)
-mse_loss.to(device)
+    def training_step(self, batch, batch_idx):
+        sequences = [item["sequence"] for item in batch]
+        graphs = Batch.from_data_list([item["graph"] for item in batch]).to(self.device)
+        y_ki = torch.tensor(
+            [item["ki"] for item in batch], dtype=torch.float32, device=self.device
+        ).unsqueeze(1)
+        y_ic50 = torch.tensor(
+            [item["ic50"] for item in batch], dtype=torch.float32, device=self.device
+        ).unsqueeze(1)
+        y_kd = torch.tensor(
+            [item["kd"] for item in batch], dtype=torch.float32, device=self.device
+        ).unsqueeze(1)
+        y_ec50 = torch.tensor(
+            [item["ec50"] for item in batch], dtype=torch.float32, device=self.device
+        ).unsqueeze(1)
+        y_classification = torch.tensor(
+            [item["is_false_ligand"] for item in batch],
+            dtype=torch.float32,
+            device=self.device,
+        ).unsqueeze(1)
 
-esm_model = esm_model.to(esm_device)
-model.to(device)
-loss_classifier.to(device)
+        with torch.autocast(device_type="cuda", dtype=torch.float16):
+            yhat_ki, yhat_ic50, yhat_kd, yhat_ec50, yhat_classification = self(
+                sequences, graphs
+            )
 
-optimiser = torch.optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=1e-3)
+        yhat_classification = torch.clamp(yhat_classification, min=-100, max=100)
 
-scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimiser, T_max=iterations)
+        ki_loss = self.loss_fn(yhat_ki, y_ki)
+        ic50_loss = self.loss_fn(yhat_ic50, y_ic50)
+        kd_loss = self.loss_fn(yhat_kd, y_kd)
+        ec50_loss = self.loss_fn(yhat_ec50, y_ec50)
+        classification_loss = self.loss_classifier(
+            yhat_classification, y_classification
+        )
 
-scaler = torch.cuda.amp.GradScaler()
+        total_loss = ki_loss + ic50_loss + kd_loss + ec50_loss + classification_loss
 
-def forward_one_step (sequences, graphs):
+        self.log("Loss/train", total_loss, on_step=True, on_epoch=True, prog_bar=True)
 
-    encoded_input = esm_tokeniser(sequences, padding="longest", return_tensors="pt")
-    esm_output = esm_model.forward(**encoded_input.to(esm_device), output_hidden_states=True)
-    hidden_states = esm_output.hidden_states
+        return total_loss
 
-    hidden_states = [x.detach().to(device) for x in hidden_states]
+    def validation_step(self, batch, batch_idx):
+        sequences = [item["sequence"] for item in batch]
+        graphs = Batch.from_data_list([item["graph"] for item in batch]).to(self.device)
+        y_ki = torch.tensor(
+            [item["ki"] for item in batch], dtype=torch.float32, device=self.device
+        ).unsqueeze(1)
+        y_ic50 = torch.tensor(
+            [item["ic50"] for item in batch], dtype=torch.float32, device=self.device
+        ).unsqueeze(1)
+        y_kd = torch.tensor(
+            [item["kd"] for item in batch], dtype=torch.float32, device=self.device
+        ).unsqueeze(1)
+        y_ec50 = torch.tensor(
+            [item["ec50"] for item in batch], dtype=torch.float32, device=self.device
+        ).unsqueeze(1)
+        y_classification = torch.tensor(
+            [item["is_false_ligand"] for item in batch],
+            dtype=torch.float32,
+            device=self.device,
+        ).unsqueeze(1)
 
-    attention_mask = encoded_input["attention_mask"].to(device)
-    return model.forward(graphs, hidden_states, attention_mask)
+        with torch.autocast(device_type="cuda", dtype=torch.float16):
+            yhat_ki, yhat_ic50, yhat_kd, yhat_ec50, yhat_classification = self(
+                sequences, graphs
+            )
 
-with torch.backends.cuda.sdp_kernel(enable_flash=True, enable_math=False, enable_mem_efficient=False):
+        yhat_classification = torch.clamp(yhat_classification, min=-100, max=100)
 
-    for i in range(iterations):
+        ki_loss = self.loss_fn(yhat_ki, y_ki)
+        ic50_loss = self.loss_fn(yhat_ic50, y_ic50)
+        kd_loss = self.loss_fn(yhat_kd, y_kd)
+        ec50_loss = self.loss_fn(yhat_ec50, y_ec50)
+        classification_loss = self.loss_classifier(
+            yhat_classification, y_classification
+        )
 
-        try:
+        total_loss = ki_loss + ic50_loss + kd_loss + ec50_loss + classification_loss
 
-            all_loss = list()
-            all_rmse = list()
+        # Log validation loss
+        self.log(
+            "Loss/validation", total_loss, on_step=False, on_epoch=True, prog_bar=True
+        )
 
-            all_y_classifications = list()
-            all_yhat_classifications = list()
+        # Compute additional metrics
+        yhat_bin_classification = (
+            torch.sigmoid(yhat_classification).detach().cpu().numpy()
+        )
+        y_classification_np = y_classification.detach().cpu().numpy()
 
-            for j in range(grad_accumulation_steps):
+        ap = average_precision_score(
+            y_classification_np.flatten(), yhat_bin_classification.flatten()
+        )
+        auroc = roc_auc_score(
+            y_classification_np.flatten(), yhat_bin_classification.flatten()
+        )
+        accuracy = accuracy_score(
+            y_classification_np.flatten(), np.round(yhat_bin_classification.flatten())
+        )
 
-                current_batch = dataset.get_train_batch(amount=batch_size)
+        # Log metrics
+        self.log("AUROC/validation", auroc, on_step=False, on_epoch=True)
+        self.log("AP/validation", ap, on_step=False, on_epoch=True)
+        self.log("Accuracy/validation", accuracy, on_step=False, on_epoch=True)
 
-                sequences = [x[0] for x in current_batch]
-                graphs = Batch.from_data_list([x[1] for x in current_batch]).detach().to(device)
+    def configure_optimizers(self):
+        optimizer = torch.optim.AdamW(
+            self.model.parameters(), lr=self.learning_rate, weight_decay=1e-3
+        )
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+            optimizer, T_max=self.iterations
+        )
+        return [optimizer], [scheduler]
 
-                y_ki = torch.from_numpy(np.asarray([x[2] for x in current_batch])).to(torch.float).unsqueeze(1).to(device)
-                y_ic50 = torch.from_numpy(np.asarray([x[3] for x in current_batch])).to(torch.float).unsqueeze(1).to(device)
-                y_kd = torch.from_numpy(np.asarray([x[4] for x in current_batch])).to(torch.float).unsqueeze(1).to(device)
-                y_ec50 = torch.from_numpy(np.asarray([x[5] for x in current_batch])).to(torch.float).unsqueeze(1).to(device)
-                y_classification = torch.from_numpy(np.asarray([x[6] for x in current_batch])).to(torch.float).unsqueeze(1).to(device)
 
-                with torch.autocast(device_type="cuda", dtype=torch.float16):
-                    yhat_ki, yhat_ic50, yhat_kd, yhat_ec50, yhat_classification = forward_one_step(sequences, graphs)
+batch_size = 16
 
-                yhat_classification = torch.clamp(yhat_classification, min=-100, max=100)
+train_sampler = None
+val_sampler = None
 
-                ki_loss = loss(yhat_ki, y_ki)
-                ic50_loss = loss(yhat_ic50, y_ic50)
-                kd_loss = loss(yhat_kd, y_kd)
-                ec50_loss = loss(yhat_ec50, y_ec50)
-                classification_loss = loss_classifier(yhat_classification, y_classification)
+if torch.cuda.device_count() > 1:
+    train_sampler = DistributedSampler(dataset.train_dataset)
+    val_sampler = DistributedSampler(dataset.validation_dataset)
 
-                current_loss = ki_loss + ic50_loss + kd_loss + ec50_loss + classification_loss
+train_loader = DataLoader(
+    dataset.train_dataset,
+    batch_size=batch_size,
+    sampler=train_sampler,
+    num_workers=4,
+    collate_fn=lambda x: x,
+)
 
-                ki_rmse = torch.sqrt(mse_loss(yhat_ki, y_ki))
-                ic50_rmse = torch.sqrt(mse_loss(yhat_ic50, y_ic50))
-                kd_rmse = torch.sqrt(mse_loss(yhat_kd, y_kd))
-                ec50_rmse = torch.sqrt(mse_loss(yhat_ec50, y_ec50))
+val_loader = DataLoader(
+    dataset.validation_dataset,
+    batch_size=batch_size,
+    sampler=val_sampler,
+    num_workers=4,
+    collate_fn=lambda x: x,
+)
 
-                current_rmse = ki_rmse + ic50_rmse + kd_rmse + ec50_rmse
+trainer = pl.Trainer(
+    devices=4,
+    accelerator="gpu",
+    strategy="ddp",
+    max_epochs=10,
+    precision=16,
+    log_every_n_steps=50,
+    logger=pl.loggers.TensorBoardLogger("logs/"),
+)
 
-                yhat_bin_classification = yhat_classification.sigmoid().detach().cpu().numpy()
-                
-                all_y_classifications.append(y_classification.cpu().numpy())
-                all_yhat_classifications.append(yhat_bin_classification)
-
-                all_loss.append(current_loss.detach())
-                all_rmse.append(current_rmse.detach())
-
-                scaler.scale(current_loss / grad_accumulation_steps).backward()
-
-            scaler.step(optimiser)
-            scaler.update()
-            optimiser.zero_grad()
-
-            number_positives = int(np.sum(np.asarray(all_y_classifications)))
-
-            print(i, sum(all_loss) / len(all_loss))
-
-            ap = average_precision_score(np.asarray(all_y_classifications).flatten(), np.asarray(all_yhat_classifications).flatten())
-            auroc = roc_auc_score(np.asarray(all_y_classifications).flatten(), np.asarray(all_yhat_classifications).flatten())
-            accuracy = accuracy_score(np.asarray(all_y_classifications).flatten(), np.round(np.asarray(all_yhat_classifications).flatten()))
-
-            writer.add_scalar("Loss/train", sum(all_loss) / len(all_loss), i)
-            writer.add_scalar("RMSE/train", sum(all_rmse) / number_positives, i)
-
-            writer.add_scalar("AUROC/train", auroc, i)
-            writer.add_scalar("AP/train", ap, i)
-            writer.add_scalar("Accuracy/train", accuracy, i)
-
-            writer.add_scalar("Hyperparams/learning_rate", scheduler.get_last_lr()[0], i)
-            writer.add_scalar("Hyperparams/temperature", model.temperature, i)
-
-            scheduler.step()
-
-            if i % 10 == 0:
-
-                all_loss = list()
-                all_rmse = list()
-
-                all_y_classifications = list()
-                all_yhat_classifications = list()
-
-                for j in range(grad_accumulation_steps):
-
-                    current_batch = dataset.get_validation_batch(amount=batch_size)
-
-                    sequences = [x[0] for x in current_batch]
-                    graphs = Batch.from_data_list([x[1] for x in current_batch]).detach().to(device)
-
-                    y_ki = torch.from_numpy(np.asarray([x[2] for x in current_batch])).to(torch.float).unsqueeze(1).to(device)
-                    y_ic50 = torch.from_numpy(np.asarray([x[3] for x in current_batch])).to(torch.float).unsqueeze(1).to(device)
-                    y_kd = torch.from_numpy(np.asarray([x[4] for x in current_batch])).to(torch.float).unsqueeze(1).to(device)
-                    y_ec50 = torch.from_numpy(np.asarray([x[5] for x in current_batch])).to(torch.float).unsqueeze(1).to(device)
-                    y_classification = torch.from_numpy(np.asarray([x[6] for x in current_batch])).to(torch.float).unsqueeze(1).to(device)
-
-                    with torch.autocast(device_type="cuda", dtype=torch.float16):
-                        yhat_ki, yhat_ic50, yhat_kd, yhat_ec50, yhat_classification = forward_one_step(sequences, graphs)
-
-                    ki_loss = loss(yhat_ki, y_ki)
-                    ic50_loss = loss(yhat_ic50, y_ic50)
-                    kd_loss = loss(yhat_kd, y_kd)
-                    ec50_loss = loss(yhat_ec50, y_ec50)
-                    classification_loss = loss_classifier(yhat_classification, y_classification)
-
-                    yhat_classification = torch.clamp(yhat_classification, min=-100, max=100)
-
-                    classification_loss = loss_classifier(yhat_classification, y_classification)
-
-                    current_loss = ki_loss + ic50_loss + kd_loss + ec50_loss + classification_loss
-
-                    ki_rmse = torch.sqrt(mse_loss(yhat_ki, y_ki))
-                    ic50_rmse = torch.sqrt(mse_loss(yhat_ic50, y_ic50))
-                    kd_rmse = torch.sqrt(mse_loss(yhat_kd, y_kd))
-                    ec50_rmse = torch.sqrt(mse_loss(yhat_ec50, y_ec50))
-
-                    current_rmse = ki_rmse + ic50_rmse + kd_rmse + ec50_rmse
-
-                    yhat_bin_classification = yhat_classification.sigmoid().detach().cpu().numpy()
-                
-                    all_y_classifications.append(y_classification.cpu().numpy())
-                    all_yhat_classifications.append(yhat_bin_classification)
-
-                    all_loss.append(current_loss.detach())
-                    all_rmse.append(current_rmse.detach())
-
-                number_positives = int(np.sum(np.asarray(all_y_classifications)))
-
-                ap = average_precision_score(np.asarray(all_y_classifications).flatten(), np.asarray(all_yhat_classifications).flatten())
-                auroc = roc_auc_score(np.asarray(all_y_classifications).flatten(), np.asarray(all_yhat_classifications).flatten())
-                accuracy = accuracy_score(np.asarray(all_y_classifications).flatten(), np.round(np.asarray(all_yhat_classifications).flatten()))
-
-                writer.add_scalar("Loss/validation", sum(all_loss) / len(all_loss), i)
-                writer.add_scalar("RMSE/validation", sum(all_rmse) / number_positives, i)
-
-                writer.add_scalar("AUROC/validation", auroc, i)
-                writer.add_scalar("AP/validation", ap, i)
-                writer.add_scalar("Accuracy/validation", accuracy, i)
-
-        except Exception as error:
-
-            print(error)
-            continue
-        
-        if i % 1000 == 0 and i != 0:
-            torch.save(model, f"saves/model_{i}.pth")
-
-    torch.save(model, "saves/model_final.pth")
+model = ConditionModelLightning()
+trainer.fit(model, train_loader, val_loader)
+trainer.save_checkpoint("saves/model_final.ckpt")
